39cf0cee8db319028771f086de8fd630bbc07b0a
Victor Bogado da Silva Lins <victor@bogado.net>
2026-02-28 14:04:59 -0500

    Remove unused functions

--- src/Registry.hpp
+++ src/Registry.hpp
@@ -6,11 +6,9 @@ extern FILE * registry;	// our opened file
 
 bool    RegLoadString (const char *,const char *,bool,char**,std::uint32_t);
 bool    RegLoadValue (const char *,const char *,bool,std::uint32_t *);
-bool	RegLoadBool(const char *,const char *,bool,bool *);
 
 void    RegSaveString (const char *,const char *,bool,const char *);
 void    RegSaveValue (const char *,const char *,bool,std::uint32_t);
-void    RegSaveBool (const char *,const char *,bool,bool);
 
 char	*php_trim(char *c, int len);	// trimming string like PHP function trim does!
 
